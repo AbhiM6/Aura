499f7481d3bb668b06fd046858b105f1119a9f80
AbhiM6 <66238840+AbhiM6@users.noreply.github.com>
2023-10-15 21:55:28 +0530

    132 Finished

---
+++ Source/Aura/Private/AbilitySystem/Data/CharacterClassInfo.cpp
@@ -0,0 +1,9 @@
+// Copyright Voidpeak Games
+
+
+#include "AbilitySystem/Data/CharacterClassInfo.h"
+
+FCharacterClassDefaultInfo UCharacterClassInfo::GetClassDefaultInfo(ECharacterClass CharacterClass)
+{
+	return CharacterClassInformation.FindChecked(CharacterClass);
+}
