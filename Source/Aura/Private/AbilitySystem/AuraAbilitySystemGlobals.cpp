82773817b3f6f73bf32e4d2ed1aea55b1354f5a3
AbhiM6 <66238840+AbhiM6@users.noreply.github.com>
2023-10-17 13:43:30 +0530

    153 Done

---
+++ Source/Aura/Private/AbilitySystem/AuraAbilitySystemGlobals.cpp
@@ -0,0 +1,11 @@
+// Copyright Voidpeak Games
+
+
+#include "AbilitySystem/AuraAbilitySystemGlobals.h"
+
+#include "AuraAbilityTypes.h"
+
+FGameplayEffectContext* UAuraAbilitySystemGlobals::AllocGameplayEffectContext() const
+{
+	return new FAuraGameplayEffectContext();
+}
