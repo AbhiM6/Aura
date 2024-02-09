892f332280b455bd8c5403889579f4cecba3eb0b
AbhiM6 <66238840+AbhiM6@users.noreply.github.com>
2024-02-09 17:45:24 +0530

    279 Done

---
+++ Source/Aura/Private/UI/WidgetController/SpellMenuWidgetController.cpp
@@ -0,0 +1,14 @@
+// Copyright Voidpeak Games
+
+
+#include "UI/WidgetController/SpellMenuWidgetController.h"
+
+void USpellMenuWidgetController::BroadcastInitialValues()
+{
+
+}
+
+void USpellMenuWidgetController::BindCallbacksToDependencies()
+{
+
+}
